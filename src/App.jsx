bb41ab30953ae03bd0d6b847a74b9c51d23ae14b
nicciccone1205 <nicciccone1205@gmail.com>
2025-05-28 18:25:05 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { useGame } from "./GameContext";
+
+import Field from "./Field";
+import Scoreboard from "./Scoreboard";
+import Welcome from "./Welcome";
+
 export default function App() {
-  return <></>;
+  const { playing } = useGame();
+  return (
+    <>
+      <h1>Whack a Mole</h1>
+      {playing ? (
+        <>
+          <Scoreboard />
+          <Field />
+        </>
+      ) : (
+        <Welcome />
+      )}
+    </>
+  );
 }
